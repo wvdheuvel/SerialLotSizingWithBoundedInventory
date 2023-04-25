8dcf45a57ac07efc61e5ad0b23850ff66cda1530
wvdheuvel <wvandenheuvel@ese.eur.nl>
2023-04-25 10:41:33 +0200

    Delete Files 10-12-16 directory

--- Files 10-12-16/cplexMLS.h
+++
@@ -1,9 +0,0 @@
-#ifndef CPLEXMLS_H
-#define CPLEXMLS_H
-
-// solve MLS with CPLEX
-void solveMLSbyCplex(Data *inst, sol_char *solMIP);
-
-#endif
-
-
--- Files 10-12-16/solchar.cpp
+++
@@ -1,9 +0,0 @@
-#include "solchar.h"
-
-// constructor
-sol_char::sol_char()
-{	double_sourcing=false;
-	double_sourcing_far=false;
-	obj=-1.0;
-	reg_network=0;
-}
